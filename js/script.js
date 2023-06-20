66514465b8c4ece5bd28156cdf3f877b26e4d2f3
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-20 15:22:08 +0200

    start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 console.log("Vue ok", Vue);
+const { createApp } = Vue;
+const app = createApp({
+  data() {
+    return {
+      todos: [
+        {
+          text: "osservare le frogghe",
+          done: false,
+        },
+        {
+          text: "saltare 3 volte",
+          done: false,
+        },
+        {
+          text: "fare andare la lavastoviglie",
+          done: false,
+        },
+        {
+          text: "andare dal medico",
+          done: false,
+        },
+        {
+          text: "sistemare la camera",
+          done: false,
+        },
+        {
+          text: "leggi una pagina di libro",
+          done: false,
+        },
+      ],
+    };
+  },
+});
+app.mount("#root");
